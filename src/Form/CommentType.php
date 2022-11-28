9ad0417d78bcca349dd40a6ad2cff3ecacbbcfd1
Manon Le Bars <manonlebars@Mac-mini-de-Manon.local>
2022-11-28 11:11:25 +0100

    create comment form

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -2,41 +2,30 @@
 
 namespace App\Form;
 
-use App\Entity\Post;
+use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class PostType extends AbstractType
+class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('title', TextType::class, [
-            'label' => 'Titre du post',
-            'constraints' => 
-                new NotBlank(),
-        ])
         ->add('body', TextareaType::class, [
             'label' => 'Contenu du post',
             'constraints' => 
                 new NotBlank(),
         ])
-        ->add('picture', TextType::class, [
-            'label' => 'photo',
-            'required' => false,
-        ])
-        
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Post::class,
+            'data_class' => Comment::class,
         ]);
     }
 }
