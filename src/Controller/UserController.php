8db10df3c5b5707cd48fbefb770a85037645714f
Manon Le Bars <lebarsmanon1@egmail.com>
2023-04-05 11:21:07 +0200

    access to profil

---
+++ src/Controller/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/profil", name="user_")
+*/
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/{id}", name="read",  requirements={"id"="\d+"})
+     */
+    public function read(User $user): Response
+    {
+        return $this->render('user/read.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+    * @Route("/modifier/{id}", name="edit", requirements={"id"="\d+"}, methods={"GET","POST"})
+    */
+    public function edit(User $user, Request $request): Response
+    {
+        
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $this->getDoctrine()->getManager()->flush();
+            
+            return $this->redirectToRoute('user_read', ['id' => $user->getId()]);
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
+        ]);
+    }
+}
