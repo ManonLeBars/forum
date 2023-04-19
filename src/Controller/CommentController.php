4b42f8a0c118632a086e256038bdf0fb8436a475
Manon Le Bars <lebarsmanon1@egmail.com>
2023-04-19 14:49:06 +0200

    delete my own comment

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,12 +14,30 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommentController extends AbstractController
 {
     /**
-     * @Route("", name="browse")
+     * @Route("/supprimer/{id}", name="delete", requirements={"id"="\d+"})
      */
-    public function browse(): Response
+    public function delete(Comment $comment, Request $request): Response
     {
-        return $this->render('comment/browse.html.twig', [
-            'controller_name' => 'CommentController',
-        ]);
+
+        if ($comment->getUser() !== $this->getUser()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        //Check if the token is available
+        $token = $request->request->get('_token');
+        if ($this->isCsrfTokenValid('deleteComment', $token)) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($comment);
+            $em->flush();
+            //Display success message
+            $this->addFlash(
+                'success',
+                'Le commentaire a été supprimé'
+            );
+
+            return $this->redirectToRoute('post_browse');
+        }
+        // If the token is not available, browse error message
+        throw $this->createAccessDeniedException('Veuillez essayer de nouveau');
     }
 }
